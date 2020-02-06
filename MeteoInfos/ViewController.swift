8ef3f59c73f9fecd1b4f5044c084aee3d06827ab
Michael Coqueret <mike@MacBook-Pro-de-Michael.local>
2020-02-06 14:28:23 +0100

    First

---
+++ MeteoInfos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeteoInfos
+//
+//  Created by Michael Coqueret on 06/02/2020.
+//  Copyright © 2020 Nicolas Moranny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
